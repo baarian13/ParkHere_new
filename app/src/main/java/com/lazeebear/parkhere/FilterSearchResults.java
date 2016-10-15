8f44b75b3252475900db0f21cf91d1a11c0222c0
Ryosuke Mitsubayashi <mitsubay@usc.edu>
2016-10-15 13:19:24 -0700

    Added Review and Search Activities

--- app/src/main/java/com/lazeebear/parkhere/FilterSearchResults.java
+++ app/src/main/java/com/lazeebear/parkhere/FilterSearchResults.java
@@ -25,8 +25,8 @@ public class FilterSearchResults extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton searchButton = (FloatingActionButton) findViewById(R.id.search_button);
+        searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
