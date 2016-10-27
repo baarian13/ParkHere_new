5adf851f07df3744648afd7671df1d62f861441b
Zhicheng Wang <zhichenw@usc.edu>
2016-10-27 00:30:04 -0700

    Add ForgotPassword page

--- app/src/main/java/com/lazeebear/parkhere/MainActivity.java
+++ app/src/main/java/com/lazeebear/parkhere/MainActivity.java
@@ -32,4 +32,9 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, PaymentActivity.class);
         startActivity(intent);
     }
+
+    public void openForgotPasswordActivity(View view){
+        Intent intent = new Intent(this, ForgotPasswordActivity.class);
+        startActivity(intent);
+    }
 }
