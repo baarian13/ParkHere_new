0e0a1ee606e3b89369705f6bdffffbfa1e065817
Ryosuke Mitsubayashi <mitsubay@usc.edu>
2016-10-31 20:18:53 -0700

    Add files via upload
    add method for sending review to server

--- app/src/main/java/com/lazeebear/parkhere/AddReview.java
+++ app/src/main/java/com/lazeebear/parkhere/AddReview.java
@@ -2,6 +2,12 @@ package com.lazeebear.parkhere;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+
+import com.lazeebear.parkhere.DAOs.SentObjects.RateDAO;
+import com.lazeebear.parkhere.ServerConnector.ServerConnector;
 
 public class AddReview extends AppCompatActivity {
 
@@ -9,5 +15,17 @@ public class AddReview extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_review);
+        Button submit = (Button) findViewById(R.id.submitReviewUser);
+        submit.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                addReviewUser();
+            }
+        });
+    }
+
+    private void addReviewUser() {
+        RatingBar ratingBar = (RatingBar) findViewById(R.id.rating);
+        int rate = ratingBar.getNumStars();
+        ServerConnector.addReviewUser(rate);
     }
 }
