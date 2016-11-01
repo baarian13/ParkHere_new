95b9833c64d1a32fa17c2b014131cab850b73f90
Henry Levy <henrylev@users.noreply.github.com>
2016-10-31 23:35:45 -0700

    Add files via upload
    files

--- app/src/main/java/com/lazeebear/parkhere/ServerConnector/ServerConnector.java
+++ app/src/main/java/com/lazeebear/parkhere/ServerConnector/ServerConnector.java
@@ -1,28 +1,18 @@
 package com.lazeebear.parkhere.ServerConnector;
 
-<<<<<<< HEAD
-=======
+
 import android.os.AsyncTask;
->>>>>>> 3f8e165b74392e0a47a3fc528453db4c71feb8f4
 import android.util.Base64;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.deser.Deserializers;
-import com.lazeebear.parkhere.DAOs.ReturnedObjects.ReturnedUserDAO;
-import com.lazeebear.parkhere.DAOs.ReturnedObjects.SpotDAO;
-import com.lazeebear.parkhere.DAOs.ReturnedObjects.SpotListDAO;
-import com.lazeebear.parkhere.DAOs.SentObjects.BookDAO;
-import com.lazeebear.parkhere.DAOs.SentObjects.RateDAO;
-import com.lazeebear.parkhere.DAOs.SentObjects.SentSpotDAO;
-import com.lazeebear.parkhere.DAOs.SentObjects.SentUserDAO;
 
-import org.json.JSONObject;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.lazeebear.parkhere.DAOs.ReturnedObjects.SpotDAO;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
+import java.lang.reflect.Type;
 import java.net.CookieManager;
 import java.net.HttpCookie;
 import java.net.HttpURLConnection;
@@ -55,8 +45,152 @@ public class ServerConnector {
         }
     }
 
-<<<<<<< HEAD
-=======
+    static class GetTokenTask extends AsyncTask<Void,Void,Void>
+       {
+                String token;
+                boolean done = false;
+                boolean success = false;
+
+                        public GetTokenTask(){
+                    }
+
+                        protected void onPreExecute() {
+                        //display progress dialog.
+
+                                    }
+                protected Void doInBackground(Void... params) {
+                        try {
+                                String url = "http://35.160.111.133:8888/get/token";
+                                URL obj = new URL(url);
+                                HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+                                con.setRequestMethod("GET");
+                                con.setRequestProperty("UserAgent", USER_AGENT);
+                                setConnCookies(con);
+                //			con.connect();
+                                        int responseCode = con.getResponseCode();
+                                System.out.println("\nSending 'GET' request to URL : " + url);
+                                System.out.println("Response Code : " + responseCode);
+
+
+                                                BufferedReader in = new BufferedReader(
+                                                new InputStreamReader(con.getInputStream()));
+                                String inputLine;
+                                StringBuffer response = new StringBuffer();
+
+                                        while ((inputLine = in.readLine()) != null) {
+                                        response.append(inputLine);
+                                    }
+                                in.close();
+                                token = response.toString();
+                                //print result
+                                        success = true;
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        done = true;
+                        return null;
+                    }
+
+
+
+                                        protected void onPostExecute(Void result) {
+                        // dismiss progress dialog and update ui
+                            }
+            }
+
+                static class SearchSpotTask extends AsyncTask<Void,Void,Void>
+        {
+                List<SpotDAO> spots;
+                String address;
+                boolean done = false;
+                boolean success = false;
+
+                        public SearchSpotTask(String address){
+                        this.address = address;
+                    }
+
+                        protected void onPreExecute() {
+                        //display progress dialog.
+
+                                    }
+                protected Void doInBackground(Void... params) {
+                        try {
+                                String url = "http://35.160.111.133:8888/search/spot?address="+ address.replace(' ', '+');
+                                URL obj = new URL(url);
+                                HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+                                con.setRequestMethod("GET");
+                                con.setRequestProperty("User-Agent", USER_AGENT);
+                                setConnCookies(con);
+                                con.connect();
+                                int responseCode = con.getResponseCode();
+                                System.out.println("\nSending 'GET' request to URL : " + url);
+                                System.out.println("Response Code : " + responseCode);
+
+
+                                                BufferedReader in = new BufferedReader(
+                                                new InputStreamReader(con.getInputStream()));
+                                String inputLine;
+                                StringBuffer response = new StringBuffer();
+
+                                        while ((inputLine = in.readLine()) != null) {
+                                        response.append(inputLine);
+                                    }
+                                in.close();
+
+                            Gson gson = new Gson();
+                                Type typeOfT = new TypeToken<List<SpotDAO>>(){}.getType();
+                                spots = gson.fromJson(response.toString(), typeOfT);
+                                //print result
+                                        success = true;
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        done = true;
+                        return null;
+                    }
+
+
+            protected void onPostExecute(Void result) {
+
+                        // dismiss progress dialog and update ui
+                            }
+            }
+
+    public static List<SpotDAO> SearchSpotTask(String address) throws Exception {
+                SearchSpotTask s = new SearchSpotTask(address);
+                s.execute();
+                while(!s.done)
+                        ;
+                if(s.success)
+                        return s.spots;
+                return null;
+            }
+
+                /*
+     Success  200 returned
+     Failure  401 returned
+      */
+                /*
+     public static int sigin(String email, String password) {
+         String url = Configs.baseURL + Configs.signinEndpoint + "?email=" + email + "&password=" + password;
+
+         RestTemplate restTemplate = new RestTemplate();
+         ResponseEntity entity = restTemplate.getForEntity(url, Object.class);
+
+         return entity.getStatusCode().value();
+     }
+     */
+
+    public static String getToken() {
+                GetTokenTask s = new GetTokenTask();
+                s.execute();
+                while(!s.done)
+                        ;
+                if(s.success)
+                        return s.token;
+                return null;
+            }
+
     static class SignInTask extends AsyncTask<Void,Void,Void>
     {
         String email;
@@ -131,77 +265,7 @@ public class ServerConnector {
         }
     }
 
->>>>>>> 3f8e165b74392e0a47a3fc528453db4c71feb8f4
-    /*
-    Success - 200 returned
-    Failure - 401 returned
-     */
-    /*
-    public static int sigin(String email, String password) {
-        String url = Configs.baseURL + Configs.signinEndpoint + "?email=" + email + "&password=" + password;
-
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity entity = restTemplate.getForEntity(url, Object.class);
-
-        return entity.getStatusCode().value();
-    }
-    */
     public static boolean signin(String email, String password) {
-<<<<<<< HEAD
-        try {
-            String url = "http://35.160.111.133:8888/signin";
-            URL obj = new URL(url);
-            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-
-            //add reuqest header
-            con.setRequestMethod("POST");
-            con.setRequestProperty("User-Agent", USER_AGENT);
-            con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
-
-            String urlParameters = "email=" + email + "&password=" + password;
-
-            // Send post request
-            con.setDoOutput(true);
-            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-            wr.writeBytes(urlParameters);
-            wr.flush();
-            wr.close();
-            Map<String, List<String>> headerFields = con.getHeaderFields();
-            List<String> cookiesHeader = headerFields.get(COOKIES_HEADER);
-
-            if (cookiesHeader != null) {
-                for (String cookie : cookiesHeader) {
-                    msCookieManager.getCookieStore().add(null, HttpCookie.parse(cookie).get(0));
-                }
-            }
-            int responseCode = con.getResponseCode();
-            System.out.println("\nSending 'POST' request to URL : " + url);
-            System.out.println("Post parameters : " + urlParameters);
-            System.out.println("Response Code : " + responseCode);
-
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(con.getInputStream()));
-            String inputLine;
-            StringBuffer response = new StringBuffer();
-
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-            }
-            in.close();
-
-            if (response.toString().trim().equals("authentication failed"))
-                return false;
-
-            //print result
-            System.out.println(response.toString());
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
-        return true;
-    }
-
-=======
         SignInTask s = new SignInTask(email, password);
         s.execute();
         while(!s.done)
@@ -305,7 +369,6 @@ public class ServerConnector {
     }
 
 
->>>>>>> 3f8e165b74392e0a47a3fc528453db4c71feb8f4
     /*
     Success - 200 returned
     Partial Success - 206 returned (Profile photo submission unsuccessful
@@ -322,65 +385,6 @@ public class ServerConnector {
     }
     */
     public static int signup(String email, String password, String first, String last, String phone, int seeker, int owner, Base64 profilePic) {
-<<<<<<< HEAD
-
-        try {
-            String url = "http://35.160.111.133:8888/signup";
-            URL obj = new URL(url);
-            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-
-            //add reuqest header
-            con.setRequestMethod("POST");
-            con.setRequestProperty("User-Agent", USER_AGENT);
-            con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
-
-            String urlParameters = null;
-
-            if (profilePic != null) {
-                urlParameters = "email=" + email + "&password=" + password + "&first=" + first +
-                        "&last=" + last + "&phone=" + phone + "&seeker=" + seeker + "&owner=" + owner + "&profilePic="
-                        + profilePic.toString();
-            } else {
-                urlParameters = "email=" + email + "&password=" + password + "&first=" + first +
-                        "&last=" + last + "&phone=" + phone + "&seeker=" + seeker + "&owner=" + owner;
-            }
-
-            // Send post request
-            con.setDoOutput(true);
-            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-            wr.writeBytes(urlParameters);
-            wr.flush();
-            wr.close();
-            Map<String, List<String>> headerFields = con.getHeaderFields();
-            List<String> cookiesHeader = headerFields.get(COOKIES_HEADER);
-
-            if (cookiesHeader != null) {
-                for (String cookie : cookiesHeader) {
-                    msCookieManager.getCookieStore().add(null, HttpCookie.parse(cookie).get(0));
-                }
-            }
-            int responseCode = con.getResponseCode();
-            System.out.println("\nSending 'POST' request to URL : " + url);
-            System.out.println("Post parameters : " + urlParameters);
-            System.out.println("Response Code : " + responseCode);
-
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(con.getInputStream()));
-            String inputLine;
-            StringBuffer response = new StringBuffer();
-
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-            }
-            in.close();
-
-            return responseCode;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return 401;
-        }
-    }
-=======
         SignUpTask s = new SignUpTask(email, password, first, last, phone, seeker, owner, profilePic);
         s.execute();
         while(!s.done)
@@ -483,5 +487,4 @@ public class ServerConnector {
             return 401;
     }
 
->>>>>>> 3f8e165b74392e0a47a3fc528453db4c71feb8f4
 }
