700218b1f4da2c29778c7fe9743957bf1f9394fd
Ryosuke Mitsubayashi <mitsubay@usc.edu>
2016-11-06 00:17:46 -0700

    add debug

--- app/src/main/java/com/lazeebear/parkhere/MainActivity.java
+++ app/src/main/java/com/lazeebear/parkhere/MainActivity.java
@@ -6,11 +6,17 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private boolean DEBUG_MODE = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (!DEBUG_MODE){
+            Intent intent = new Intent(this,LoginActivity.class);
+            startActivity(intent);
+        }
     }
 
     public void openSignInActivity(View view) {
