de8b21d728d2fedbd56646eb9cf6b449ed7e4cb6
Sally Wei <sallywei@usc.edu>
2016-11-06 19:12:53 -0800

    Adds loading page
    Hides the shortcut buttons when not in debug mode (sets buttons to
    default invisible and changes them to visible when their actions are
    called). Makes DEBUG_MODE static so that it can be called from tests
    without having to instantiate a MainActivity. Adds DEBUG_MODE setter.

--- app/src/main/java/com/lazeebear/parkhere/MainActivity.java
+++ app/src/main/java/com/lazeebear/parkhere/MainActivity.java
@@ -1,12 +1,14 @@
 package com.lazeebear.parkhere;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private boolean DEBUG_MODE = true;
+    private static boolean DEBUG_MODE = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,9 +16,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         if (!DEBUG_MODE){
-            Intent intent = new Intent(this,LoginActivity.class);
-            startActivity(intent);
+            //make the thread wait a bit so the loading page can show up
+            int sec = 1000; //1 second
+            openLoginPageAfterWaiting(sec);
+        } else {
+            setButtonsToVisible();
         }
+    }
+
+    private void openLoginPageAfterWaiting(int sec){
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                //do this after "sec" seconds
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        }, sec);
+    }
+
+    private void setButtonsToVisible() {
+        (findViewById(R.id.mainpage_sign_in_button)).setVisibility(View.VISIBLE);
+        (findViewById(R.id.mainpage_search_button)).setVisibility(View.VISIBLE);
+        (findViewById(R.id.mainpage_account_button)).setVisibility(View.VISIBLE);
+        (findViewById(R.id.mainpage_payment_button)).setVisibility(View.VISIBLE);
+        (findViewById(R.id.mainpage_forgot_pasword_button)).setVisibility(View.VISIBLE);
     }
 
     public void openSignInActivity(View view) {
@@ -43,4 +67,7 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, ForgotPasswordActivity.class);
         startActivity(intent);
     }
+
+    public static void setDebugMode(boolean inDebugMode){ DEBUG_MODE = inDebugMode; }
+    public static boolean getDebugMode () { return DEBUG_MODE; }
 }
