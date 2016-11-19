38f594931d34a261a648abaffa9040ad05afcf63
Ryosuke Mitsubayashi <mitsubay@usc.edu>
2016-11-18 19:49:06 -0800

    fixed transaction finished page

--- app/src/main/java/com/lazeebear/parkhere/CompleteTransactionActivity.java
+++ app/src/main/java/com/lazeebear/parkhere/CompleteTransactionActivity.java
@@ -7,23 +7,30 @@ import android.view.View;
 import android.widget.Button;
 
 public class CompleteTransactionActivity extends AppCompatActivity {
+    String uniqueID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_complete_transaction);
 
-        Button confirmButton = (Button) findViewById(R.id.confirmButton_complete_trans);
-        confirmButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                backToAccountPage();
-            }
-        });
+        Intent intent = getIntent();
+        if (intent != null) {
+            uniqueID = intent.getStringExtra("id");
+
+            Button confirmButton = (Button) findViewById(R.id.confirmButton_complete_trans);
+            confirmButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    backToAccountPage();
+                }
+            });
+        }
     }
 
     private void backToAccountPage() {
         Intent intent = new Intent(this, Account.class);
+        intent.putExtra("id",uniqueID);
         startActivity(intent);
     }
 }
