22fcd3f932b03bd9ace3fcd22049c11871a668d6
Ryosuke Mitsubayashi <mitsubay@usc.edu>
2016-11-27 23:39:59 -0800

    cache for login (speed performance)

--- app/src/main/java/com/lazeebear/parkhere/LoginActivity.java
+++ app/src/main/java/com/lazeebear/parkhere/LoginActivity.java
@@ -4,6 +4,7 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -67,9 +68,23 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private View mProgressView;
     private View mLoginFormView;
     private static int verified = 0; // 0 for true, 1 for false. static so tests can toggle it with a static method.
+    private String prefName = "User Data";
+    private String prefEmail = "email";
+    private String prefPassword = "password";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //check for cache
+        SharedPreferences prefs = getSharedPreferences(prefName,0);
+        String email = prefs.getString(prefEmail,"");
+        String password = prefs.getString(prefPassword,"");
+        //System.out.println("Prefs: " + email + " and " + password);
+        if (email != "" && password != ""){
+
+            login(email, password);
+        }
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         // Set up the login form.
@@ -231,34 +246,45 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             // Show a progress spinner, and kick off a background task to
             // perform the user login attempt.
 //            showProgress(true);
-            try {
-                if (ServerConnector.signin(email, password)) {
-                    try {
-                        { // check if the user is verified by querying the server for (all of) the user's details.
-                            //ReturnedUserDAO userInfo = ServerConnector.userDetails(email);
-                            //verified = userInfo.getIsVerified(); // TODO: Make userDAO Parcelable to not double-query the server
-                            Log.i("STATE", "finished signing in / getting user details");
-                        }
-                        verified = 1; //0 is false, 1 is true
-                        if (verified == 1) { //if verified
-                            Log.i("STATE", "user is verified");
-                            Intent intent = new Intent(this, Account.class);
-                            //setUserInformation();
-                            intent.putExtra("id", email);
-                            Log.i("STATE", "starting account activity");
-                            startActivity(intent);
-                        } else {
-                            Log.i("STATE", "user is not verified");
-                            Intent intent = new Intent(this, UserVerificationActivity.class);
-                            startActivity(intent);
-                        }
-                    } catch (Exception e) {
-                        Log.i("STATE", "Error while logging in: exception when getting user details from server.");
+            login(email, password);
+        }
+    }
+
+    private void login(String email, String password){
+        try {
+            if (ServerConnector.signin(email, password)) {
+                try {
+                    { // check if the user is verified by querying the server for (all of) the user's details.
+                        //ReturnedUserDAO userInfo = ServerConnector.userDetails(email);
+                        //verified = userInfo.getIsVerified(); // TODO: Make userDAO Parcelable to not double-query the server
+                        Log.i("STATE", "finished signing in / getting user details");
                     }
+                    verified = 1; //0 is false, 1 is true
+                    if (verified == 1) { //if verified
+                        Log.i("STATE", "user is verified");
+                        //cache
+                        SharedPreferences prefs = getSharedPreferences(prefName, 0);
+                        SharedPreferences.Editor editor = prefs.edit();
+                        editor.putString(prefEmail, email);
+                        editor.putString(prefPassword, password);
+                        editor.commit();
+
+                        Intent intent = new Intent(this, Account.class);
+                        //setUserInformation();
+                        intent.putExtra("id", email);
+                        Log.i("STATE", "starting account activity");
+                        startActivity(intent);
+                    } else {
+                        Log.i("STATE", "user is not verified");
+                        Intent intent = new Intent(this, UserVerificationActivity.class);
+                        startActivity(intent);
+                    }
+                } catch (Exception e) {
+                    Log.i("STATE", "Error while logging in: exception when getting user details from server.");
                 }
-            } catch (Exception e){
-                Log.i("ERROR", "Exception while signing in");
             }
+        } catch (Exception e){
+            Log.i("ERROR", "Exception while signing in");
         }
     }
 
