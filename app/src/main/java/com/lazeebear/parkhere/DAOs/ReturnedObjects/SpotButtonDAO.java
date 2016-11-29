62292ae92e8b59acfa4830b2203793712df480b4
Ryosuke Mitsubayashi <mitsubay@usc.edu>
2016-11-28 16:28:11 -0800

    front end for spot history (improved speed)

--- app/src/main/java/com/lazeebear/parkhere/DAOs/ReturnedObjects/SpotButtonDAO.java
+++ app/src/main/java/com/lazeebear/parkhere/DAOs/ReturnedObjects/SpotButtonDAO.java
@@ -8,8 +8,8 @@ public class SpotButtonDAO {
     private int ID;
     private String address;
 
-    private int getID(){return this.ID;}
-    private void setID(int id){this.ID = id;}
-    private String getAddress(){return this.address;}
-    private void setAddress(String address){this.address = address;}
+    public int getID(){return this.ID;}
+    public void setID(int id){this.ID = id;}
+    public String getAddress(){return this.address;}
+    public void setAddress(String address){this.address = address;}
 }
